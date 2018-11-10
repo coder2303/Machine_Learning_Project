df573054508a1c89a334e60b62506d08774dfd77
coder2303 <44891318+coder2303@users.noreply.github.com>
2018-11-11 01:42:04 +0530

    Update hand_written_digit_recognition_dnn.py

--- hand_written_digit_recognition_dnn.py
+++ hand_written_digit_recognition_dnn.py
@@ -1,5 +1,3 @@
-
-
 import glob
 import math
 import os
@@ -16,28 +14,27 @@ from sklearn import metrics
 import tensorflow as tf
 from tensorflow.python.data import Dataset
 
-tf.logging.set_verbosity(tf.logging.ERROR)
-pd.options.display.max_rows = 10
-pd.options.display.float_format = '{:.1f}'.format
-
+# reading the input file as CSV
 digit_dataframe=pd.read_csv("https://download.mlcc.google.com/mledu-datasets/mnist_train_small.csv",
                                                                                             sep=",",
                                                                                             header=None)
-digit_dataframe=digit_dataframe.head(10000)
-
+digit_dataframe=digit_dataframe.head(10000) 
 digit_dataframe=digit_dataframe.reindex(np.random.permutation(digit_dataframe.index))
 
+
+# scaling and labeling the neccesary features
 def scale_label_and_features(dataset):
   label=dataset[0]
-  
   features=dataset.loc[:,1:784]
   features=features/255
   
   return features,label
 
+# spliting the input into training and validation data
 training_examples,trainig_targets=scale_label_and_features(digit_dataframe[:7500])
 validation_examples,validation_targets=scale_label_and_features(digit_dataframe[7500:10000])
 
+# creating input function for model to train 
 def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuffle=True):
 
     idx = np.random.permutation(features.index)
@@ -54,20 +51,18 @@ def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuf
     feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
     return feature_batch, label_batch
 
+#input function for test model 
 def create_predict_input_fn(features, labels, batch_size):
-
-
     raw_features = {"pixels": features.values}
     raw_targets = np.array(labels)
-    
     ds = Dataset.from_tensor_slices((raw_features, raw_targets)) 
     ds = ds.batch(batch_size)
-    
-        
     # Return the next batch of data.
     feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
     return feature_batch, label_batch
 
+
+#training our DNN classifier
 def train_neural_network_model(
     learning_rate,
     hidden_units,
@@ -115,7 +110,6 @@ def train_neural_network_model(
     input_fn=training_input_fn,
     steps=steps_per_period)
     
-    
     training_prediction=list(classifier.predict(input_fn=predict_training_input_fn))
     training_prob=np.array([item['probabilities'] for item in training_prediction])
     training_pred_class_id = np.array([item['class_ids'][0] for item in training_prediction])
@@ -132,21 +126,15 @@ def train_neural_network_model(
     training_loss.append(training_log_loss)
     validation_loss.append(validation_log_loss)
     
-      # Calculate final predictions (not probabilities, as above).
+  # Calculate final predictions (not probabilities, as above).
   final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
   final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
-  
-  print(len(final_predictions))
-  
   accuracy = metrics.accuracy_score(validation_targets, final_predictions)
   print("Final accuracy (on validation data): %0.2f" % accuracy)
-  
-  f1=metrics.f1_score(validation_targets,final_predictions,average='micro')
-  
-  print(f1.astype("float"))
-    
+
   return classifier
 
+# getting the train classifier
 classifier=train_neural_network_model(
     learning_rate=0.05,
     hidden_units=[100,100],
@@ -157,14 +145,14 @@ classifier=train_neural_network_model(
     validation_examples=validation_examples,
     validation_targets=validation_targets)
 
+# getting the test input 
 mnist_test_dataframe = pd.read_csv(
   "https://download.mlcc.google.com/mledu-datasets/mnist_test.csv",
   sep=",",
   header=None)
 
-
+# splitting and testing the data on train classifier
 test_examples,test_target=scale_label_and_features(mnist_test_dataframe)
-
 predict_test_input_fn = lambda:create_predict_input_fn(
        test_examples, test_target, batch_size=100)  
 
@@ -175,15 +163,9 @@ accuracy = metrics.accuracy_score(test_target, test_predictions)
 print("Final accuracy (on validation data): %0.2f" % accuracy)
   
 f1_test=metrics.f1_score(test_target,test_predictions,average='micro')
-  
-print(f1_test.astype("float"))
 
-print(classifier.get_variable_names())
-
+# showing the output 
 weights0 = classifier.get_variable_value("dnn/hiddenlayer_0/kernel")
-
-print("weights0 shape:", weights0.shape)
-
 num_nodes = weights0.shape[1]
 num_rows = int(math.ceil(num_nodes / 10.0))
 fig, axes = plt.subplots(num_rows, 10, figsize=(20, 2 * num_rows))
